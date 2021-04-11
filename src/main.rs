31919adaae6f3eba305123ad426b4e29f9806a93
Jeff Hughes <jeff.hughes@gmail.com>
2021-04-11 19:09:15 -0400

    Create bare-bones REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::{
+    io::{stdin, stdout, Write},
+    process::exit,
+};
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let mut input_buffer = String::new();
+        print_prompt();
+        stdin()
+            .read_line(&mut input_buffer)
+            .expect("Error reading input");
+        if input_buffer.trim() == ".exit" {
+            exit(0);
+        } else {
+            println!("Unrecognized command {}.", input_buffer.trim());
+        }
+    }
+}
+
+fn print_prompt() {
+    print!("db > ");
+    stdout().flush().unwrap();
 }
